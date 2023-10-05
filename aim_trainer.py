fec8f789b1e1fe2e6eeeaeb485e8cf1dcf60f246
phrenkie-caibi <95527652+phrenkie@users.noreply.github.com>
2023-10-05 15:39:13 +0200

    something

--- aim_trainer.py
+++ aim_trainer.py
@@ -1,26 +1,18 @@
 import pyautogui
-import time
 import keyboard
-import random
 
 def aim_trainer():
-    pyautogui.PAUSE = 0.001
+    pyautogui.PAUSE = 0.1
     while True:
         if keyboard.is_pressed('q'):
             print("Exiting program...")
             break
-        
-        search_region = (776, 134, 1748, 672)  # Adjust coordinates as needed
-        confidence_threshold = 0.6  # Adjust confidence as needed
-        
-        target_location = pyautogui.locateOnScreen('target.png', region=search_region, confidence=confidence_threshold)
+        search_region = (776, 134, 1748, 672) 
+        target_location = pyautogui.locateOnScreen('target.png', region=search_region, confidence=0.8)
         
         if target_location is not None:
             target_x, target_y = pyautogui.center(target_location)
             pyautogui.click(target_x, target_y)
         
-        # Minimize sleep time between iterations
-        time.sleep(0.001)  # Sleep for 1 millisecond or adjust as needed
-
 if __name__ == "__main__":
     aim_trainer()
